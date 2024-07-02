d751a6065d39f1a889a7063c4f7636dc7f86fda4
80Hydrargyrum <rg-.s@usp.br>
2024-07-02 11:18:37 -0300

    Atualização 02/09/2024

--- lab_bank.py
+++ lab_bank.py
@@ -1,10 +1,41 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base # gambiarra
 from markupsafe import escape
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f5f3e9a5875dabc5b96c185ecec1cd374bdfcf67deecef1c9de698799fe0cabce89e19de1159f3a1a73da9f4070f9e1bcfd77c61c876102a6353d268993a8060fc743350c9efebc1e3e66373a1b4f020'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    email = db.Column(db.String(200), unique = True, nullable = False)
+    profile_picture = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60))
+    posts = db.relationship('Post', backref = 'author', lazy = True)
+    
+    def __repr__(self):
+        
+        return f"User('{self.username}', '{self.email}', '{self.profile_picture}')"
+
+class Post(db.Model):
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
+    def __repr__(self):
+        
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 @app.route('/')
 @app.route('/home')
